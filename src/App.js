642ec7306f3400e6ea75d6de08761a4e1d1fb29f
Vandhana-Mohan <mohan.vandhana@gmail.com>
2023-02-26 09:18:45 -0500

    Add logs

--- src/App.js
+++ src/App.js
@@ -1,27 +1,28 @@
-import { useState, useEffect } from 'react';
-import './App.css';
+import { useState, useEffect } from "react";
+import "./App.css";
 
 const API_BASE = "https://dummyjson.com/";
 
 function App() {
-  const [ product, setProduct ] = useState([])
+  const [product, setProduct] = useState([]);
 
   useEffect(() => {
-    function getProductInfo(){
+    function getProductInfo() {
       const url = `${API_BASE}/products/`;
-        fetch(url)
-          .then((result) => result.json())
-          .then((data) => {
-            console.log(data)
-            setProduct(data)   
-          });
+      fetch(url)
+        .then((result) => result.json())
+        .then((data) => {
+          console.log(data);
+          setProduct(data);
+        });
     }
-      getProductInfo()
-  },[]);
-   
+    getProductInfo();
+  }, []);
+
   return (
     <div>
-      
+      <h1> hello </h1>
+      <h1 class="text-3xl font-bold underline">Hello world!</h1>
     </div>
   );
 }
---
+++ team_logs/Vandhana.md
@@ -0,0 +1,6 @@
+
+02/26/2022
+
+- installed tailwind
+- fetched api in app.js - useEffect hook
+- displayed products with title for jewellery shop

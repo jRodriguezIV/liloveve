327ad5417b6cc86599edc71341d9fc7ae3d59011
Vandhana-Mohan <mohan.vandhana@gmail.com>
2023-02-26 10:41:04 -0500

    Display Images and View Details button

--- src/App.js
+++ src/App.js
@@ -14,8 +14,10 @@ function App() {
       fetch(url)
         .then((result) => result.json())
         .then((data) => {
-          console.log(data);
-          setProduct(data);
+          setProduct(data.products);
+        })
+        .catch((error) => {
+          console.error("Error fetching product data:", error);
         });
     }
     getProductInfo();
@@ -23,9 +25,13 @@ function App() {
 
   return (
     <div>
-      <h1> hello </h1>
-      <h1 className="text-3xl font-bold underline">Hello world!</h1>
-      <Shop />
+      <h1> hello without tailwind </h1>
+      <h1 className="text-3xl font-bold underline">
+        Hello world! with tailwind{" "}
+      </h1>
+      <main>
+        <Shop product={product} />
+      </main>
     </div>
   );
 }
--- src/Components/Shop.js
+++ src/Components/Shop.js
@@ -1,7 +1,32 @@
+import { useState } from "react";
 
-const Shop = () => {
-    console.log("hi")
-    return null
-}
+const Shop = ({ product }) => {
+  console.log("hellooooo", { product });
+  const [selectedProduct, setSelectedProduct] = useState(null);
 
-export default Shop
+  function handleViewDetails(product) {
+    setSelectedProduct(product);
+  }
+
+  return (
+    <div>
+      <h1> Shop </h1>
+      {product.map((products) => {return (
+        <div key={products.id}>
+          <img src={products.images[0]} alt={products.brand} />
+          <button onClick={() => handleViewDetails(products)}> View Details</button>
+        </div>
+      )})}
+      {selectedProduct && (
+        <div>
+          <h2> {selectedProduct.brand}</h2>
+          <p> {selectedProduct.category}</p>
+          <p>{selectedProduct.description}</p>
+          <p>{selectedProduct.price}</p>
+        </div>
+      )}
+    </div>
+  );
+};
+
+export default Shop;

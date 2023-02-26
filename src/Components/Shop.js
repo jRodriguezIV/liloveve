8f4e1d01998384a7c7a855d5b60aebb402f2dc12
Vandhana-Mohan <mohan.vandhana@gmail.com>
2023-02-26 14:55:27 -0500

    edit grid

--- src/Components/Shop.js
+++ src/Components/Shop.js
@@ -3,15 +3,14 @@ import { useState } from "react";
 const Shop = ({ product }) => {
   const [selectedProduct, setSelectedProduct] = useState(null);
 
-  //   function handleViewDetails(products) {
-  //     console.log("hi", products);
-  //     setSelectedProduct(products);
-  //   }
-
+  function handleViewDetails(products) {
+    setSelectedProduct(products);
+  }
+  
   return (
     <div className="bg-yellow-100">
       <h1> Shop </h1>
-      <div className="grid grid-cols-5 grid-rows-6 gap-4 mb-2 ">
+      <div className="grid grid-cols-5 grid-rows-6 gap-2 mb-2 ">
         {product.map((products) => {
           return (
             <div key={products.id}>
@@ -23,15 +22,22 @@ const Shop = ({ product }) => {
                 />
 
                 <div className="absolute top-0 left-0 right-0 bottom-0 flex items-center justify-center opacity-0 hover:opacity-100 transition-opacity duration-300">
-                  
                   <p className="text-black">{products.description}</p>
                 </div>
               </span>
-              {/* <button onClick={() => handleViewDetails(products)} View Details </button> */}
+              <button onClick={() => handleViewDetails(products)}> {" "} View Details{" "} </button>
             </div>
           );
         })}
       </div>
+      {selectedProduct && (
+        <div className="flex flex-col items-center mt-2 h-16">
+        <button onClick={() => setSelectedProduct(null)}>Close</button>
+        <p className="text-red-600">Price: {selectedProduct.price}</p>
+      </div>
+      
+      )}
+      
     </div>
   );
 };

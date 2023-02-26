ec91e920adf814fe86158c50467274b6234682ad
Vandhana-Mohan <mohan.vandhana@gmail.com>
2023-02-26 13:23:45 -0500

    edit grid

--- src/Components/Shop.js
+++ src/Components/Shop.js
@@ -11,7 +11,7 @@ const Shop = ({ product }) => {
   return (
     <div className="bg-yellow-100">
       <h1> Shop </h1>
-      <div className="grid grid-cols-4 grid-rows-3 gap-4 mb-2">
+      <div className="grid grid-cols-5 grid-rows-6 gap-4 mb-2 ">
         {product.map((products) => {
           return (
             <div key={products.id}>
@@ -19,7 +19,7 @@ const Shop = ({ product }) => {
                 <img
                   src={products.images[0]}
                   alt={products.brand}
-                  className="cursor-pointer border-solid border-2 rounded border-red-100 max-h-300 max-w-300"
+                  className="cursor-pointer border-solid border-2 rounded border-red-100 max-h-40 max-w-xs m-auto p-auto"
                 />
 
                 <div className="absolute top-0 left-0 right-0 bottom-0 flex items-center justify-center opacity-0 hover:opacity-100 transition-opacity duration-300">

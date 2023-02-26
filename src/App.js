b360be874d16bbe28792976f24d7887623b8676f
Vandhana-Mohan <mohan.vandhana@gmail.com>
2023-02-26 09:13:03 -0500

    Add tailwind

--- src/App.js
+++ src/App.js
@@ -1,10 +1,27 @@
+import { useState, useEffect } from 'react';
 import './App.css';
 
+const API_BASE = "https://dummyjson.com/";
+
 function App() {
+  const [ product, setProduct ] = useState([])
+
+  useEffect(() => {
+    function getProductInfo(){
+      const url = `${API_BASE}/products/`;
+        fetch(url)
+          .then((result) => result.json())
+          .then((data) => {
+            console.log(data)
+            setProduct(data)   
+          });
+    }
+      getProductInfo()
+  },[]);
+   
   return (
     <div>
-      Hello Harold!
-      Hello Keeanu!
+      
     </div>
   );
 }

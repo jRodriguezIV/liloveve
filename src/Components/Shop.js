672c76122dcc01f1d85e0716c07b7a51a03bc3ee
Vandhana-Mohan <mohan.vandhana@gmail.com>
2023-02-26 11:09:04 -0500

    fix error in display image

--- src/Components/Shop.js
+++ src/Components/Shop.js
@@ -1,30 +1,26 @@
 import { useState } from "react";
 
 const Shop = ({ product }) => {
-  console.log("hellooooo", { product });
   const [selectedProduct, setSelectedProduct] = useState(null);
 
-  function handleViewDetails(product) {
-    setSelectedProduct(product);
-  }
+  //   function handleViewDetails(products) {
+  //     console.log("hi", products);
+  //     setSelectedProduct(products);
+  //   }
 
   return (
     <div>
       <h1> Shop </h1>
-      {product.map((products) => {return (
-        <div key={products.id}>
-          <img src={products.images[0]} alt={products.brand} />
-          <button onClick={() => handleViewDetails(products)}> View Details</button>
-        </div>
-      )})}
-      {selectedProduct && (
-        <div>
-          <h2> {selectedProduct.brand}</h2>
-          <p> {selectedProduct.category}</p>
-          <p>{selectedProduct.description}</p>
-          <p>{selectedProduct.price}</p>
-        </div>
-      )}
+      {product.map((products) => {
+        return (
+          <div key={products.id} className ="grid-cols-4">
+            <span>
+            <img src={products.images[0]} alt={products.brand} />
+            </span>
+            {/* <button onClick={() => handleViewDetails(products)} View Details </button> */}
+          </div>
+        );
+      })}
     </div>
   );
 };

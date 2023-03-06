bf761d3203e2ac64028704f431d9531e2f5944bc
jRodriguezIV <j.a.rodrigueziv@gmail.com>
2023-03-06 00:00:34 -0500

    added react router, Home and Carousel Components, styling , horiztonal scrolling product display

---
+++ src/Components/Carousel/Carousel.js
@@ -0,0 +1,15 @@
+import "./Carousel.css";
+export default function Carousel({ item }) {
+  return (
+    <div>
+      <img src={`${item.images[0]}`} alt={`${item.title}`} />
+      <div className="foot">
+        <p>
+          <strong>{item.title}</strong>
+        </p>
+        <p className="description">{`${item.brand}, ${item.category}`}</p>
+        <p style={{ paddingTop: 12 }}>{`$${item.price}`}</p>
+      </div>
+    </div>
+  );
+}
